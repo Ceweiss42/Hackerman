e1615081e3590c33fac4b99db313d1a1cbff64ee
Ceweiss42 <42663413+Ceweiss42@users.noreply.github.com>
2020-04-03 15:22:45 -0700

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,7 +1,31 @@
+import BoolManager
+
+hintUsed = "false"
+
+def useHint():
+    print("\nYou used a hint!")
+    inventory.remove("Hint Machine")
+    BoolManager.hintUsed = BoolManager.level
+
 def openInventory():
     print("Inventory:")
     for i in range(len(inventory)):
-        print(inventory[i])
+        print("|||" + inventory[i] + "|||"),
+    
+    choice = raw_input("\nWhat would you like to use?")
+    if(choice.lower() == "file" or choice.lower() == "target info file"):
+        openInfo()
+    elif choice.lower() == "hint" or choice.lower() == "hint machine":
+        if("Hint Machine" in inventory):
+            useHint()
 
-hints = 3
-inventory = ["Empty", "Empty", "Empty"]
+
+inventory = []
+name = ""
+
+def openInfo():
+    print(" _______________________________________")
+    BoolManager.print_slow("| Name: Vladimir Levin     Age: 25      |\n", .06)
+    BoolManager.print_slow("| IP:12.544.178            Host:pop3.ru |\n", .06)
+    BoolManager.print_slow("| Brother: Yogi                         |\n", .06)
+    print(" _______________________________________")
